7140e39c8cbda8e430d16d7dfc2e4ceeda0aa24a
Alex Johnstone <alexjj@gmail.com>
2022-11-06 16:56:42 +0000

    wordcoutn

--- app.py
+++ app.py
@@ -1,18 +1,5 @@
 import streamlit as st
-
-cam=st.selectbox(
-  'Choose a Cam', 
-  [
-    '',
-    'pitriverbridge/pitriverbridge.jpg',
-    'johnsongrade/johnsongrade.jpg',
-    'perez/perez.jpg',
-    'mthebron/mthebron.jpg',
-    'eurekaway/eurekaway.jpg',
-    'sr70us395/sr70us395.jpg',
-    'bogard/bogard.jpg',
-    'eastriverside/eastriverside.jpg',
-  ]
-)
-if cam:
-  st.image('https://cwwp2.dot.ca.gov/data/d2/cctv/image/' + cam)
+text = st.text_area('Type or paste some  text')
+if text:
+  words = text.split()
+  st.write(f'Number of words in your text:\n\n{len(words)}')
