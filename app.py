5e6488408ba89fb707e03932ade21dde8de934aa
Alex Johnstone <alexjj@gmail.com>
2022-11-06 16:55:49 +0000

    camera

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import streamlit as st
 
+cam=st.selectbox(
+  'Choose a Cam', 
+  [
+    '',
+    'pitriverbridge/pitriverbridge.jpg',
+    'johnsongrade/johnsongrade.jpg',
+    'perez/perez.jpg',
+    'mthebron/mthebron.jpg',
+    'eurekaway/eurekaway.jpg',
+    'sr70us395/sr70us395.jpg',
+    'bogard/bogard.jpg',
+    'eastriverside/eastriverside.jpg',
+  ]
+)
+if cam:
+  st.image('https://cwwp2.dot.ca.gov/data/d2/cctv/image/' + cam)
